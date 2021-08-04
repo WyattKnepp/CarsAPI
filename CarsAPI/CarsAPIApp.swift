d3dae476d22c081bd2cab76de75ea55eb6ef197d
Wyatt Knepp <wyattjoeknepp@gmail.com>
2021-08-04 01:59:46 -0500

    Initial Commit

---
+++ CarsAPI/CarsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsAPIApp.swift
+//  CarsAPI
+//
+//  Created by Wyatt Knepp on 8/4/21.
+//
+
+import SwiftUI
+
+@main
+struct CarsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
